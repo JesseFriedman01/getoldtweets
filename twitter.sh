c891d53f596fce1787390c82d6721caf480ab093
Jesse Friedman <35785851+JesseFriedman01@users.noreply.github.com>
2020-06-28 10:30:41 -0400

    Add files via upload

---
+++ twitter.sh
@@ -0,0 +1,6 @@
+sudo yum install python38 python38-pip
+sudo yum install git -y
+mkdir twitter
+cd twitter
+git clone https://github.com/JesseFriedman01/getoldtweets.git
+pip3 install --user getoldtweets3 ftfy 
